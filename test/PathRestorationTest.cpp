74bf3e4f974dea7c7ee9defb20f41c851c142878
Da Li <njulida@outlook.com>
2020-04-15 21:59:11 +0800

    Suceessfully run the first naive GTest code.

--- test/PathRestorationTest.cpp
+++ test/PathRestorationTest.cpp
@@ -4,6 +4,10 @@
 
 #include "gtest/gtest.h"
 
+TEST(TmpTest, NotZero) {
+  ASSERT_EQ(0, 0);
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
