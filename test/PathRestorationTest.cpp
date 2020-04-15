b349c03057a4ad0236b6c9fc3a5bfbdfe6fb36e8
Da Li <njulida@outlook.com>
2020-04-15 22:18:20 +0800

    Add JSON library into testing

--- test/PathRestorationTest.cpp
+++ test/PathRestorationTest.cpp
@@ -2,13 +2,19 @@
 // Created by lida on 2020/4/15.
 //
 
+#include "../src/core/PathRestoration.h"
 #include "gtest/gtest.h"
+#include <fstream>
 
-TEST(TmpTest, NotZero) {
-  ASSERT_EQ(0, 0);
+std::string inputPath = "../inputs/test-data-with-oracle-20200327.txt";
+
+TEST(PathRestorationTestSuite, RegressionTest) {
+  //TODO: read 6000+ test cases from ...
+
 }
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
+
   return RUN_ALL_TESTS();
 }
